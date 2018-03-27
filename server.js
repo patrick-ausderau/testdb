140a72398d661c2e8f9700e954f00a1b711eadcb
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2018-03-27 16:04:05 +0300

    no https yet

--- server.js
+++ server.js
@@ -17,22 +17,6 @@ const catSchema = new Schema({
 
 const Cat = mongoose.model('cat', catSchema);
 
-app.enable('trust proxy');
-
-// Add a handler to inspect the req.secure flag (see 
-// http://expressjs.com/api#req.secure). This allows us 
-// to know whether the request was via http or https.
-// https://github.com/aerwin/https-redirect-demo/blob/master/server.js
-app.use ((req, res, next) => {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/cat`).then(() => {
   console.log('Connected successfully.');
   app.listen(process.env.APP_PORT);
